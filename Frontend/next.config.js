a1459f240edcb108df4bdbcdae0be7b99071682a
DESKTOP-LF74LKS\Andre <andre.luizfr2@gmail.com>
2024-09-28 16:05:29 -0300

    fix: tentando rodar no netlify

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   reactStrictMode: true,
-  useFileSystemPublicRoutes: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     APP_BASE_URL: process.env.APP_BASE_URL
-  }
+  },
+  output: 'export'
 }
 
 module.exports = nextConfig
