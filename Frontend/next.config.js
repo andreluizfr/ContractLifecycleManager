a1098662b6643343da6d45eeb70d35f7a5866269
DESKTOP-LF74LKS\Andre <andre.luizfr2@gmail.com>
2024-09-28 12:53:01 -0300

    fix: tentando resolver problema roteamento errado em prod

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   reactStrictMode: true,
+  useFileSystemPublicRoutes: true,
+  swcMinify: false,
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     APP_BASE_URL: process.env.APP_BASE_URL
-  },
-  useFileSystemPublicRoutes: false,
-  swcMinify: false
+  }
 }
 
 module.exports = nextConfig
